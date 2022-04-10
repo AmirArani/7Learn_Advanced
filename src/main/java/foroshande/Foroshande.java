8dae31464257bbdbc6554575c1899647761a2227
AmirArani <blackstar.m77@gmail.com>
2022-04-10 20:21:54 +0430

    1401/01/21 | S1103 | Static blocks of code

--- src/main/java/foroshande/Foroshande.java
+++ src/main/java/foroshande/Foroshande.java
@@ -3,6 +3,15 @@ package foroshande;
 import khodro.jadid.Mashin;
 
 public class Foroshande {
+
+    public static  String[] nameForoshandegan = new String[10];
+
+    //static block
+    static {
+        nameForoshandegan[0] = "ali";
+        nameForoshandegan[1] = "ahmad";
+    }
+
     public void forosheMashin() {
 
         Mashin.tedadeForosh++;
