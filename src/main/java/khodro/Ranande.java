d102d5fa3de2b27eb195642c3021e7b31e1e396a
AmirArani <blackstar.m77@gmail.com>
2022-04-10 20:31:32 +0430

    1401/01/21 | S1104 | Static Class

--- src/main/java/khodro/Ranande.java
+++ src/main/java/khodro/Ranande.java
@@ -1,4 +1,26 @@
 package khodro;
 
+//Outer class
 public class Ranande {
+
+    private String noeGavahiname = "Paye 1"; //runtime
+    private static String name = "Farhad"; //compile time
+
+    //Non-static Nested Class -> Inner class
+    public class Gavahiname {
+        public void print(){
+            System.out.println("Class Non Static: ");
+            System.out.println(noeGavahiname);
+            System.out.println(name);
+        }
+    }
+
+    //Only nested classes could be static.
+    public static class StaticGavahiname{
+        public void print(){
+            System.out.println("Class Static: ");
+//            System.out.println(noeGavahiname);  //doesnt work
+            System.out.println(name);
+        }
+    }
 }
