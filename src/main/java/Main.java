3d899b1c25a6a432a4e96db7d517a09f119f16d2
AmirArani <blackstar.m77@gmail.com>
2022-04-12 20:07:48 +0430

    1401/01/23 | S1202 | scanner

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,7 @@
 import khodro.jadid.Mashin;
 
+import java.util.Scanner;
+
 import static java.lang.Integer.*;
 import static java.lang.System.*;
 
@@ -19,6 +21,15 @@ public class Main {
         }
 
         out.println(mashin.getMark());
+
+        Scanner scanner = new Scanner(in);
+        out.println("Esme shoma chist?");
+        String nam = scanner.nextLine();
+        out.println("Sen khod ra vared konid");
+        String sen = scanner.nextLine();
+        out.println("dar che shahri zendegei mikonid?");
+        String shahr = scanner.nextLine();
+        out.println("Man " + nam + " hastam, "+sen+" sale az "+shahr);
 
         out.println("Payane Ejra!");
     }
