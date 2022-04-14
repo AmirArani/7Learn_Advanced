a82f9ea611f09445dbb5aaaee2ecffd12f71ce43
AmirArani <blackstar.m77@gmail.com>
2022-04-14 18:04:41 +0430

    1401/01/25 | S1403 | final keyword last session

--- src/main/java/khodro/Kashti.java
+++ src/main/java/khodro/Kashti.java
@@ -3,6 +3,12 @@ package khodro;
 import khodro.jadid.Mashin;
 
 public class Kashti extends Khodro{
+
+    @Override
+    protected void protectedMethode(){
+        System.out.println("We are in Kashti Class");
+    }
+
     @Override
     public boolean AyaDarHalHarekatAst() {
         Mashin mashin = new Mashin();
